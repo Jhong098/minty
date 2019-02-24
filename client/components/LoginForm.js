e3b4421af5415cde9473430d70079b90d2a67083
Jhong098 <Jhong098@gmail.com>
2019-02-24 13:45:49 -0500

    login form styles

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+
 
 const containerStyle = css`
   padding: 10%;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const inputStyle = css`
+  margin: 20px;
+`;
+
+const buttonStyle = css`
+  margin: 20px;
+  display: flex;
+  justify-content: center;
 `;
 
 class Login extends Component {
@@ -34,32 +57,34 @@ class Login extends Component {
   render() {
     return (
       <div className="container" css={containerStyle}>
-        <h2 style={{marginBottom: '40px'}}>Login</h2>
-        <form onSubmit={ this.handleSubmit }>
+        <h2 style={{ marginBottom: '40px' }}>Login</h2>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <input
+            <Input
               type="email"
               placeholder="Email"
               className="form-control"
               name="email"
               onChange={ this.handleInputChange }
               value={ this.state.email }
+              css={inputStyle}
             />
           </div>
           <div className="form-group">
-            <input
+            <Input
               type="password"
               placeholder="Password"
               className="form-control"
               name="password"
               onChange={ this.handleInputChange }
               value={ this.state.password }
+              css={inputStyle}
             />
           </div>
           <div className="form-group">
-            <button type="submit" className="btn btn-primary">
+            <Button css={buttonStyle} variant="contained" type="submit" color="primary">
               Login
-            </button>
+            </Button>
           </div>
         </form>
       </div>
