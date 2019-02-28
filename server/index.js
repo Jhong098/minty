89ea75ce783edc30a3475125526a20b9026ca765
Jhong098 <Jhong098@gmail.com>
2019-02-27 23:18:51 -0500

    WIP SSR

--- server/index.js
+++ server/index.js
@@ -1,47 +1,43 @@
 import Express from 'express';
-import compression from 'compression';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import path from 'path';
-import IntlWrapper from '../client/modules/Intl/IntlWrapper';
+
+// local imports
+import serverConfig from './config';
+import dummyData from './dummyData';
+const webpackConfig = require('../webpack.config.dev')[0];
+const SSR = require('./SSR');
 
 // Initialize the Express App
 const app = new Express();
 
 // Set Development modes checks
-const isDevMode = process.env.NODE_ENV === 'development' || false;
-const isProdMode = process.env.NODE_ENV === 'production' || false;
+const isDevMode = serverConfig.nodeEnv === 'development';
+const isTest = serverConfig.nodeEnv === 'test';
 
 // Run Webpack dev server in development mode
 if (isDevMode) {
-  // Webpack Requirements
-  // eslint-disable-next-line global-require
   const webpack = require('webpack');
-  // eslint-disable-next-line global-require
-  const config = require('../webpack.config.dev');
-  // eslint-disable-next-line global-require
   const webpackDevMiddleware = require('webpack-dev-middleware');
-  // eslint-disable-next-line global-require
   const webpackHotMiddleware = require('webpack-hot-middleware');
-  const compiler = webpack(config);
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath,
-    watchOptions: {
-      poll: 1000,
+
+  const middlewareOptions = {
+    stats: { colors: true },
+    noInfo: false,
+    lazy: false,
+    headers: {
+      'Access-Control-Allow-Origin': 'http://localhost',
     },
-  }));
+    publicPath: webpackConfig.output.publicPath,
+  };
+
+  const compiler = webpack(webpackConfig);
+  const webpackDevMiddlewareInstance = webpackDevMiddleware(compiler, middlewareOptions);
+  app.use(webpackDevMiddlewareInstance);
   app.use(webpackHotMiddleware(compiler));
 }
 
-// React And Redux Setup
-import { configureStore } from '../client/store';
-import { Provider } from 'react-redux';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router-dom';
-import Helmet from 'react-helmet';
-import passport from 'passport';
 // Import required modules
 import routes from '../client/routes';
 import { fetchComponentData } from './util/fetchData';
@@ -51,132 +47,54 @@ import transaction from './routes/transaction.routes';
 import balance from './routes/balance.routes';
 import user from './routes/user.routes';
 // import dummyData from './dummyData';
-import serverConfig from './config';
 // import { fetchCategories } from './lib/fetch';
 
 // Set native promises as mongoose promise
 mongoose.Promise = global.Promise;
 
 // MongoDB Connection
-if (process.env.NODE_ENV !== 'test') {
-  mongoose.connect(serverConfig.mongoURL, { useNewUrlParser: true }, (error) => {
-    if (error) {
-      console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
-      throw error;
-    }
+mongoose.connect(isTest ? serverConfig.testMongoURL: serverConfig.mongoURL, (error) => {
+  if (error) {
+    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
+    throw error;
+  } else {
+    console.log(`Connected to DB at ${isTest ? serverConfig.testMongoURL: serverConfig.mongoURL}`)
+  }
 
-    // feed some dummy data in DB.
-    //dummyData();
+  // feed some dummy data in DB.
+  //dummyData();
 
-    // feed latest transactions and balances to DB
-    //fetchCategories();
-    writeTransactionsToDB();
-    writeBalancesToDB();
-  });
-}
+  // feed latest transactions and balances to DB
+  //fetchCategories();
+  // writeTransactionsToDB();
+  // writeBalancesToDB();
+});
+
 
 // authentication
+import passport from 'passport';
 app.use(passport.initialize());
 import auth from './passport';
 auth(passport);
 
 // Apply body Parser and server public assets and routes
-app.use(compression());
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
-app.use(Express.static(path.resolve(__dirname, '../dist/client')));
+app.use(Express.static(path.resolve(__dirname, '..', 'dist')));
+
 app.use('/api', transaction);
 app.use('/api', balance);
 app.use('/api/users', user);
 
-// Render Initial HTML
-const renderFullPage = (html, initialState) => {
-  const head = Helmet.rewind();
+app.get('*', SSR.default);
 
-  // Import Manifests
-  const assetsManifest = process.env.webpackAssets && JSON.parse(process.env.webpackAssets);
-  const chunkManifest = process.env.webpackChunkAssets && JSON.parse(process.env.webpackChunkAssets);
-
-  return `
-    <!doctype html>
-    <html>
-      <head>
-        ${head.base.toString()}
-        ${head.title.toString()}
-        ${head.meta.toString()}
-        ${head.link.toString()}
-        ${head.script.toString()}
-
-        ${isProdMode ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
-        <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
-        <link rel="stylesheet" href="https://unpkg.com/react-table@latest/react-table.css"/>
-        <link rel="shortcut icon" href="https://img.icons8.com/dusk/64/000000/us-dollar.png" type="image/png" />
-      </head>
-      <body>
-        <div id="root">${process.env.NODE_ENV === 'production' ? html : `<div>${html}</div>`}</div>
-        <script>
-          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-          ${isProdMode ?
-          `//<![CDATA[
-          window.webpackManifest = ${JSON.stringify(chunkManifest)};
-          //]]>` : ''}
-        </script>
-        <script src='${isProdMode ? assetsManifest['/vendor.js'] : '/vendor.js'}'></script>
-        <script src='${isProdMode ? assetsManifest['/app.js'] : '/app.js'}'></script>
-      </body>
-    </html>
-  `;
-};
-
-const renderError = err => {
-  const softTab = '&#32;&#32;&#32;&#32;';
-  const errTrace = isProdMode ?
-    `:<br><br><pre style="color:red">${softTab}${err.stack.replace(/\n/g, `<br>${softTab}`)}</pre>` : '';
-  return renderFullPage(`Server Error${errTrace}`, {});
-};
-
-// Server Side Rendering based on routes matched by React-router.
-app.use((req, res, next) => {
-  match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
-    if (err) {
-      return res.status(500).end(renderError(err));
+if (!isTest) {
+  // start app
+  app.listen(serverConfig.port, error => {
+    if (!error) {
+      console.log(`MERN is running on port: ${serverConfig.port}! Build something amazing!`); // eslint-disable-line
     }
-
-    if (redirectLocation) {
-      return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-    }
-
-    if (!renderProps) {
-      return next();
-    }
-
-    const store = configureStore();
-
-    return fetchComponentData(store, renderProps.components, renderProps.params)
-      .then(() => {
-        const initialView = renderToString(
-          <Provider store={store}>
-            <IntlWrapper>
-              <RouterContext {...renderProps} />
-            </IntlWrapper>
-          </Provider>
-        );
-        const finalState = store.getState();
-
-        res
-          .set('Content-Type', 'text/html')
-          .status(200)
-          .end(renderFullPage(initialView, finalState));
-      })
-      .catch((error) => next(error));
   });
-});
-
-// start app
-app.listen(serverConfig.port, (error) => {
-  if (!error) {
-    console.log(`MERN is running on port: ${serverConfig.port}! Build something amazing!`); // eslint-disable-line
-  }
-});
+}
 
 export default app;
---
+++ shared/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Feed from './containers/Feed';
+import PostView from './containers/PostView';
+
+const Routes = () => (
+  <Switch>
+    {/* <Route exact path="/" component={Feed} />
+    <Route path="/:slug" component={PostView} /> */}
+  </Switch>
+);
+
+export default Routes;
